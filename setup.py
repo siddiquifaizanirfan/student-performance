0bd5a42f403245fea31289f34a50c5088275617f
siddiquifaizanirfan <sidsiquifaizxxn459@gmail.com>
2026-07-14 22:02:29 +0530

    third commit

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 from typing import List
-hypen='-e .'
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n"," ") for req in requirements]
-        if hypen in requirements:
-            requirements.remove(hypen)
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+
+    requirements = [req.strip() for req in requirements]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
     return requirements
 
+
 setup(
-    name="student performance",
+    name="student_performance",
     version="0.0.1",
-    author="faizan",
-    author_email="sidsiquifaizxxn459@gmail.com",
+    author="Faizan",
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements("requirements.txt")
 )
+print(get_requirements("requirements.txt"))
